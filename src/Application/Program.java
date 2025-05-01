3efb48c39ee44f3f99f34afaa17f9db5b8a25bc5
lucianopovoas <paixaoluciano69@gmail.com>
2025-05-01 18:40:39 -0300

    Creation Seller entitie Class

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,6 +1,9 @@
 package Application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.util.Date;
 
 public class Program {
 
@@ -9,5 +12,7 @@ public class Program {
     Department obj = new Department(1, "Açougue");
     System.out.println(obj);
 
+    Seller seller = new Seller(4, "Pedro", "pedro@gmail.com", 7500.0, new Date(), obj);
+        System.out.println(seller);
     }
 }
